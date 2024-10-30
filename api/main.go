209fd4f67823850731fa9dbcdaa6a9846940533f
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-10-30 17:13:32 +0530

    Update main.go

--- api/main.go
+++ api/main.go
@@ -13,5 +13,6 @@ func main() {
 
 // HelloServer says hello.
 func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("received request", r.URL.Path)
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
